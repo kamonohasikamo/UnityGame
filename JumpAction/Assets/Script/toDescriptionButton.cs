959ca16b37cc5ac2043c5f1b3e5d8641f0846130
SomaHiroKS <soma.hiroki.17@shizuoka.ac.jp>
2019-12-26 17:15:15 +0900

    操作説明画面

---
+++ JumpAction/Assets/Script/toDescriptionButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class toDescriptionButton : MonoBehaviour
+{
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("Description");
+    }
+}
