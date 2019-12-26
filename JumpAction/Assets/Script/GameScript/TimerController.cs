55d651a13c9690ad16880f7fd5d378ad734f6efe
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-26 16:54:14 +0900

    TimerのUI調整とシステムの実装

---
+++ JumpAction/Assets/Script/GameScript/TimerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+	// ゲーム終了時、minuteとsecondsからトータル秒を計算すればいい
+	// totalSeconds = minute * 60 + seconds;
+	// 分
+	[SerializeField]
+	private int minute;
+	// 秒
+	[SerializeField]
+	private float seconds;
+	// preb Update Time
+	private float oldSeconds;
+	// Timer text
+	private Text timerText;
+
+	void Start()
+	{
+		minute = 0;
+		seconds = 0f;
+		oldSeconds = 0f;
+		timerText = GetComponentInChildren<Text>();
+	}
+
+	void Update()
+	{
+		seconds += Time.deltaTime;
+		if (seconds >= 60f)
+		{
+			minute++;
+			seconds -= 60;
+		}
+		// 値が変わったときにUI変更
+		if ((int)seconds != (int)oldSeconds)
+		{
+			timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+		}
+		oldSeconds = seconds;
+	}
+}
