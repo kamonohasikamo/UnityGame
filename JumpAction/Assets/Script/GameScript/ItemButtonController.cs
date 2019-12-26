e0db244cad5e486905e61eca9005d0d434439819
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-26 18:15:31 +0900

    Game右下の三色ボタンの設置＆ボタンを押すと数字カウントアップ機能の実装(ボタンを押したら減らす処理が必要)

---
+++ JumpAction/Assets/Script/GameScript/ItemButtonController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemButtonController : MonoBehaviour
+{
+	[SerializeField]
+	int redCount;
+	[SerializeField]
+	int blueCount;
+	[SerializeField]
+	int greenCount;
+
+	GameObject redButton;
+	GameObject blueButton;
+	GameObject greenButton;
+
+	void Start()
+	{
+		redCount = 0;
+		blueCount = 0;
+		greenCount = 0;
+		redButton = transform.GetChild(0).gameObject;
+		blueButton = transform.GetChild(1).gameObject;
+		greenButton = transform.GetChild(2).gameObject;
+	}
+
+	void Update()
+	{
+		if (redCount <= 9)
+		{
+			redButton.GetComponentInChildren<Text>().text = "x" + redCount.ToString();
+		}
+		if (blueCount <= 9)
+		{
+			blueButton.GetComponentInChildren<Text>().text = "x" + blueCount.ToString();
+		}
+		if (greenCount <= 9)
+		{
+			greenButton.GetComponentInChildren<Text>().text = "x" + greenCount.ToString();
+		}
+	}
+
+	public void OnClickRedButton()
+	{
+		redCount++;
+	}
+
+	public void OnClickBlueButton()
+	{
+		blueCount++;
+	}
+
+	public void OnClickGreenButton()
+	{
+		greenCount++;
+	}
+}
