7d83143ea9ef741710fdc029b9ebd547451783e3
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-27 19:07:00 +0900

    GameManagerとステージ生成＆1つだけ動く

---
+++ JumpAction/Assets/Script/GameManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	//=============================================
+	// GameManager instance
+	//=============================================
+	public static GameManager instance = null;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	//=============================================
+	// use CreateBlockController variable
+	//=============================================
+
+	// Block の横サイズ
+	public int blockHorizontalLength;
+	
+	public int getBlockHorizontalLength()
+	{
+		return blockHorizontalLength;
+	}
+
+	public void setBlockHorizontalLength(int setValue)
+	{
+		blockHorizontalLength = setValue;
+	}
+
+	// waveの最大値
+	public static int maxWave = 5;
+
+	public int getMaxWave()
+	{
+		return maxWave;
+	}
+
+	public void setMaxWave(int setValue)
+	{
+		maxWave = setValue;
+	}
+
+	// 各waveが進んでいいかどうかのFlag 
+	public static bool[] isWaveMoving = new bool[maxWave];
+	
+	public bool[] getIsWaveMoving()
+	{
+		return isWaveMoving;
+	}
+
+	public bool getOneIsWaveMoving(int getPoint)
+	{
+		return isWaveMoving[getPoint];
+	}
+
+	public void setOneIsWaveMoving(int setPoint, bool setFlag)
+	{
+		isWaveMoving[setPoint] = setFlag; 
+	}
+
+	public void setIsWaveMoving(bool[] setFlags)
+	{
+		isWaveMoving = setFlags;
+	}
+
+	// wave move speed
+	public static float waveMoveSpeed;
+
+	public float getWaveMoveSpeed()
+	{
+		return waveMoveSpeed;
+	}
+
+	public void setWaveMoveSpeed(float setValue)
+	{
+		waveMoveSpeed = setValue;
+	}
+
+	//=============================================
+	// use ItemButtonController variable and more?
+	//=============================================
+	public static int playerHaveRedCount;
+
+	public int getPlayerHaveRedCount()
+	{
+		return playerHaveRedCount;
+	}
+
+	public void setPlayerHaveRedCount(int setValue)
+	{
+		playerHaveRedCount = setValue;
+	}
+
+	public static int playerHaveBlueCount;
+
+	public int getPlayerHaveBlueCount()
+	{
+		return playerHaveBlueCount;
+	}
+
+	public void setPlayerHaveBlueCount(int setValue)
+	{
+		playerHaveBlueCount = setValue;
+	}
+
+	public static int playerHaveGreenCount;
+
+	public int getPlayerHaveGreenCount()
+	{
+		return playerHaveGreenCount;
+	}
+
+	public void setPlayerHaveGreenCount(int setValue)
+	{
+		playerHaveGreenCount = setValue;
+	}
+}
--- JumpAction/Assets/Script/GameScript/CreateBlockController.cs
+++ JumpAction/Assets/Script/GameScript/CreateBlockController.cs
@@ -5,28 +5,96 @@ using UnityEngine;
 public class CreateBlockController : MonoBehaviour
 {
 	// Blockの横幅を指定
-	int blockHorizontalLength = 5;
+	int blockHorizontalLength = 10;
+
+	// 作成するwave最大値
+	public static int maxWave = 5;
+	// Blockの高さの最大値
+	int blockMaxHight = 8;
 	// ランダム用変数
 	System.Random randamVariable = new System.Random();
 
+	// blockの位置座標を動かす親オブジェクトの配列群
+	GameObject[] parentBlock = new GameObject[maxWave];
+
 	// ブロックの高さ各様用変数
 	int blockHeight;
+
+	// 各waveが進んでいいかどうかのFlag 
+	bool[] isWaveMoving = new bool[maxWave];
+
+	// wave move speed
+	public float waveMoveSpeed = 1.0f;
+
 	void Start()
 	{
-		for (int i = 1; i < blockHorizontalLength; i++)
+		GameManager.instance.setMaxWave(6);
+		Debug.Log(GameManager.instance.getMaxWave());
+		for (int i = 0; i < maxWave; i++)
 		{
-			GameObject redBlockObject = (GameObject)Resources.Load("redBlock");
-			blockHeight = randamVariable.Next(1, 5);
-			redBlockObject.transform.localScale = new Vector3(1, blockHeight, 1);
-			// Blockの位置調整
-			float scaleY = (blockHeight == 1) ? 0.0f : (float)(blockHeight - 1) * 0.5f;
-			Instantiate(redBlockObject, new Vector2(10.0f + (float)i, scaleY), Quaternion.identity);
+			int colorSelectRandom = randamVariable.Next(0, 3);
+			createBlock(colorSelectRandom, i);
+			isWaveMoving[i] = false;
 		}
+		isWaveMoving[0] = true;
 	}
 
 	
 	void Update()
 	{
-		
+		parentBlock[0].transform.Translate(-1 * waveMoveSpeed * Time.deltaTime, 0, 0);
+		Debug.Log(parentBlock[0]);
+	}
+
+	void createBlock(int colorSelect, int waveNum)
+	{
+		parentBlock[waveNum] = (GameObject)Resources.Load("parentBlock");
+		parentBlock[waveNum].name = "parentBlock" + waveNum.ToString();
+		GameObject tmpParentObj = Instantiate(parentBlock[waveNum], new Vector2(10.0f, 0.0f), Quaternion.identity) as GameObject;
+		switch(colorSelect)
+		{
+			case 0:
+				for (int i = 0; i < blockHorizontalLength; i++)
+				{
+					GameObject redBlockObject = (GameObject)Resources.Load("redBlock");
+					blockHeight = randamVariable.Next(1, blockMaxHight);
+					redBlockObject.transform.localScale = new Vector3(1, blockHeight, 1);
+					// Blockの位置調整
+					float scaleY = (blockHeight == 1) ? 0.0f : (float)(blockHeight - 1) * 0.5f;
+					
+					GameObject tmpObject = Instantiate(redBlockObject, new Vector2(10.0f + (float)i, scaleY - 4.5f), Quaternion.identity) as GameObject;
+					
+					tmpObject.transform.parent = tmpParentObj.transform;
+				}
+				break;
+			case 1:
+				for (int i = 0; i < blockHorizontalLength; i++)
+				{
+					GameObject blueBlockObject = (GameObject)Resources.Load("blueBlock");
+					blockHeight = randamVariable.Next(1, blockMaxHight);
+					blueBlockObject.transform.localScale = new Vector3(1, blockHeight, 1);
+					// Blockの位置調整
+					float scaleY = (blockHeight == 1) ? 0.0f : (float)(blockHeight - 1) * 0.5f;
+					
+					GameObject tmpObject = Instantiate(blueBlockObject, new Vector2(10.0f + (float)i, scaleY - 4.5f), Quaternion.identity) as GameObject;
+					
+					tmpObject.transform.parent = tmpParentObj.transform;
+				}
+				break;
+			case 2:
+				for (int i = 0; i < blockHorizontalLength; i++)
+				{
+					GameObject greenBlockObject = (GameObject)Resources.Load("greenBlock");
+					blockHeight = randamVariable.Next(1, blockMaxHight);
+					greenBlockObject.transform.localScale = new Vector3(1, blockHeight, 1);
+					// Blockの位置調整
+					float scaleY = (blockHeight == 1) ? 0.0f : (float)(blockHeight - 1) * 0.5f;
+
+					GameObject tmpObject = Instantiate(greenBlockObject, new Vector2(10.0f + (float)i, scaleY - 4.5f), Quaternion.identity) as GameObject;
+					tmpObject.transform.parent = tmpParentObj.transform;
+				}
+				break;
+		}
+		parentBlock[waveNum] = tmpParentObj;
 	}
 }
