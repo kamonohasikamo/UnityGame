d8ccd07edb6d2a168aa7a1577f39a4d71c68fef0
SomaHiroKS <soma.hiroki.17@shizuoka.ac.jp>
2019-12-27 18:06:41 +0900

    HPバー

---
+++ JumpAction/Assets/Script/GameScript/HpController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//UI使うときは忘れずに。
+using UnityEngine.UI;
+
+public class HpController : MonoBehaviour
+{
+    //最大HPと現在のHP。
+    int maxHp = 100;
+    float currentHp;
+
+    public float NaturalDamege = 0.01f;
+    //Sliderを入れる
+    public Slider slider;
+
+    void Start()
+    {
+        //Sliderを満タンにする。
+        slider.value = 1;
+        //現在のHPを最大HPと同じに。
+        currentHp = maxHp;
+    }
+
+    void Update()
+    {
+        currentHp = currentHp - NaturalDamege;
+        slider.value = (float)currentHp / (float)maxHp;
+    }
+
+}
