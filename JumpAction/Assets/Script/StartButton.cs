9a6d6e4c801a9d5ebdf437f281462b7055780b2a
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-28 13:06:19 +0900

    Load画面の作成とアニメーションの追加

--- JumpAction/Assets/Script/StartButton.cs
+++ JumpAction/Assets/Script/StartButton.cs
@@ -6,8 +6,41 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-    public void ButtonClicked()
-    {
-        SceneManager.LoadScene("Game");
-    }
+	// 非同期動作で使用
+	private AsyncOperation async;
+
+	// GameStart画面のUI
+	[SerializeField]
+	public GameObject startUI;
+
+	// ロード画面で使用するUI
+	[SerializeField]
+	public GameObject loadUI;
+
+	// ロード画面のスライダー
+	[SerializeField]
+	public Slider slider;
+	
+	public void ButtonClicked()
+	{
+		startUI.SetActive(false);
+		loadUI.SetActive(true);
+		StartCoroutine("LoadGame");
+	}
+
+	IEnumerator LoadGame()
+	{
+		yield return new WaitForSeconds(1f);
+		async = SceneManager.LoadSceneAsync("Game");
+
+		// 読み込みが終わるまで進捗状況をスライダーに送る
+		while(!async.isDone)
+		{
+			var progressVal = Mathf.Clamp01(async.progress / 0.9f);
+			slider.value = progressVal;
+			yield return null;
+		}
+
+
+	}
 }
