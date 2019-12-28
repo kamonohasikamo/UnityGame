ccc95e3e2f3b87f7f63895a3aceb57e1af3f2901
SomaHiroKS <soma.hiroki.17@shizuoka.ac.jp>
2019-12-28 17:35:31 +0900

    BGM追加

---
+++ JumpAction/Assets/Script/AudioController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void PlaySE_touch()
+    {
+        audioSource.Play();
+    }
+}
