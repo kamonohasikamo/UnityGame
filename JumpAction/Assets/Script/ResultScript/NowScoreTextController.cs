07980d9df9512bbb0450e5e16720c8d546aafd30
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2019-12-28 12:03:26 +0900

    ランキング機能のシステム部作成(スコアを当てれば完成)

---
+++ JumpAction/Assets/Script/ResultScript/NowScoreTextController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NowScoreTextController : MonoBehaviour
+{
+	private Text nowScoreText;
+	void Start()
+	{
+		this.nowScoreText = GetComponent<Text>();
+		// Score表示↓
+		// this.nowScoreText.text += "<color=blue>" + + "</color>";
+	}
+}
--- JumpAction/Assets/Script/ResultScript/RankingController.cs
+++ JumpAction/Assets/Script/ResultScript/RankingController.cs
@@ -1,18 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+//==================================================
+// ランキング機能の処理
+// 5位まで表示させる場合、配列を6(= 5 + 1)個分で用意する。
+// 一旦いままでの5個のスコアと現在のスコア(計6個の値)を保存する。
+// // その後、ソートを行う。
+// ソート後、上から順にみていき
+// 上位5個を保存し、6個目を破棄。
+// (→　今回のスコアだろうが、今までのスコアだろうが、最下位が消される仕様)
+// この保存するときに、現在のスコアと一致していれば、赤色にする。
+// ただし、現在のスコアと同一の値をとった場合
+// このままだと両方赤色になってしまうので
+// Flagを用意して、1回だけ赤色にする、という処理を行えばOK
+//==================================================
 public class RankingController : MonoBehaviour
 {
-	// Start is called before the first frame update
+	// UI Text
+	private Text targetText;
+
+	// sort用配列
+	private int[] sort = new int[6];
+	private int tmp = 0;
+
 	void Start()
 	{
-		
+		this.targetText = this.GetComponent<Text>();
+		 // 初期化
+		this.targetText.text = "";
+		saveRanking();
+		showRanking();
 	}
 
-	// Update is called once per frame
-	void Update()
+	void saveRanking()
 	{
-		
+		// Scoreを端末に保存する
+		// 第一引数：保存名
+		// 第二引数：保存する値(SetIntなのでInt型限定)
+		// PlayerPrefabs.SetInt("5", );
+		// PlayerPrefabs.Save();
+	}
+
+	void showRanking()
+	{
+		this.targetText.text += "Ranking\n";
+		for (int i = 0; i < 6; i++)
+		{
+			// sort[i] = PlayerPrefabs.GetInt("" + i, 0);
+		}
+
+		// sort配列の中身を降順ソートする
+		for (int start = 1; start < sort.Length; start++)
+		{
+			for (int end = sort.Length - 1;end >= start; end--)
+			{
+				if (sort[end - 1] <= sort[end])
+				{
+					tmp = sort[end - 1];
+					sort[end - 1] = sort[end];
+					sort[end] = tmp;
+				}
+			}
+		}
+
+		bool showNowScoreFlag = true;
+		for (int i = 0; i < 5; i++)
+		{
+			// 今のスコアがソート配列の中のものと一致したら、表示させる処理
+			// if (showNowScoreFlag &&  == sort[i])
+			// {
+			// 	this.targetText.text += "<color=red>" + (i + 1) + ":" + sort[i].ToString("D5") + "</color>\n";
+			// 	PlayerPrefabs.SetInt("" + i, sort[i]);
+			// 	showNowScoreFlag = false;
+			// }
+			// else
+			// {
+			// 	this.targetText.text += (i + 1) + ":" + sort[i].ToString("D5") + "\n";
+			// 	PlayerPrefabs.SetInt("" + i, sort[i]);
+			// }
+		}
+		// PlayerPrefabs.Save();
 	}
 }
