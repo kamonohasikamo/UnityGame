b1f34cc36601c2fa417314d089ee25f3f836c035
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2020-01-03 17:13:05 +0900

    リザルト画面で星がはじけるエフェクトを付けた

---
+++ JumpAction/Assets/Script/ResultScript/ResultStarController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultStarController : MonoBehaviour
+{
+	public void EndStarAnimation()
+	{
+		var parent = this.gameObject.transform.parent.gameObject;
+		Destroy(parent);
+	}
+}
---
+++ JumpAction/Assets/Script/ResultScript/ResultStarObjectController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultStarObjectController : MonoBehaviour
+{
+	public float startTime;
+	void Start()
+	{
+		StartCoroutine(showStar());
+	}
+
+	IEnumerator showStar()
+	{
+		yield return new WaitForSeconds(startTime);
+		foreach (Transform child in transform)
+		{
+			if(child.name == "ResultStarObject")
+			{
+				child.gameObject.SetActive(true);
+			}
+		}
+	}
+
+}
