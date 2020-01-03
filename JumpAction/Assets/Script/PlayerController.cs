8015c593b2384f539b6713f5f3ce5315ac61c834
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2020-01-03 16:03:29 +0900

    ブロックに押された後、一定時間が経つと右に戻る機能の実装

--- JumpAction/Assets/Script/PlayerController.cs
+++ JumpAction/Assets/Script/PlayerController.cs
@@ -6,34 +6,40 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
 	Rigidbody2D rb2d;
-    public AudioClip sound1;
-    public AudioClip sound2;
-    AudioSource audioSource;
+	public AudioClip sound1;
+	public AudioClip sound2;
+	AudioSource audioSource;
 
-    public float maxHeight;
+	public float maxHeight;
 	public float flapVelocity;
+
+	// 何秒後に右に戻す動作をするかを指定する
+	public float rightMovingTimeThreshold;
+
+	// どれくらいの勢いで右に戻すか
+	public float RightForceMagnification;
 	int redCount = 0;
 	int blueCount = 0;
 	int greenCount = 0;
 	int healCount = 0;
 	float flyTime = 0.0f;
-	bool fly ;
+	bool fly;
 	int value;
-    int typeMatch;
+	int typeMatch;
 
+	float playerRightCorrectionTime;
 
-
-	// Start is called before the first frame update
 	void Start()
 	{
 		fly = true;
-        audioSource = gameObject.GetComponent<AudioSource>();
-        value = Random.Range(0, 3);
+		playerRightCorrectionTime = 0.0f;
+		audioSource = gameObject.GetComponent<AudioSource>();
+		value = Random.Range(0, 3);
 		switch(value)
 		{
 			case 0:
 				gameObject.GetComponent<Renderer>().material.color = Color.red;
-                break;
+				break;
 			case 1:
 				gameObject.GetComponent<Renderer>().material.color = Color.blue;
 				break;
@@ -59,6 +65,21 @@ public class PlayerController : MonoBehaviour
 			GameManager.instance.setPlayerFlyTime(flyTime);
 		}
 
+		if (fly && tmp.x < 0)
+		{
+			playerRightCorrectionTime += Time.deltaTime;
+			if (playerRightCorrectionTime > rightMovingTimeThreshold)
+			{
+				Vector3 movePos = this.gameObject.transform.position;
+				movePos.x += (Time.deltaTime) * RightForceMagnification;
+				this.gameObject.transform.position = movePos;
+			}
+		}
+		else
+		{
+			playerRightCorrectionTime = 0.0f;
+		}
+
 		if(tmp.x <= -3.5 || tmp.y <= -5.5)
 		{
 			SceneManager.LoadScene("Result");
@@ -70,8 +91,8 @@ public class PlayerController : MonoBehaviour
 		if (transform.position.y < maxHeight)
 		{
 			Flap();
-            audioSource.PlayOneShot(sound1);
-        }
+			audioSource.PlayOneShot(sound1);
+		}
 	}
 
 	public void Flap()
@@ -84,24 +105,24 @@ public class PlayerController : MonoBehaviour
 	{
 		if (col.gameObject.tag == "ColorItemRed")
 		{
-            audioSource.PlayOneShot(sound2);
-            Destroy(col.gameObject);
+			audioSource.PlayOneShot(sound2);
+			Destroy(col.gameObject);
 			redCount = GameManager.instance.getPlayerHaveRedCount();
 			redCount++;
 			GameManager.instance.setPlayerHaveRedCount(redCount);
 		}
 		else if (col.gameObject.tag == "ColorItemBlue")
 		{
-            audioSource.PlayOneShot(sound2);
-            Destroy(col.gameObject);
+			audioSource.PlayOneShot(sound2);
+			Destroy(col.gameObject);
 			blueCount = GameManager.instance.getPlayerHaveBlueCount();
 			blueCount++;
 			GameManager.instance.setPlayerHaveBlueCount(blueCount);
 		}
 		else if (col.gameObject.tag == "ColorItemGreen")
 		{
-            audioSource.PlayOneShot(sound2);
-            Destroy(col.gameObject);
+			audioSource.PlayOneShot(sound2);
+			Destroy(col.gameObject);
 			greenCount = GameManager.instance.getPlayerHaveGreenCount();
 			greenCount++;
 			GameManager.instance.setPlayerHaveGreenCount(greenCount);
@@ -115,66 +136,69 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-    void OnCollisionStay2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "RedBlock")
-        {
-            fly = false;
-            if (gameObject.GetComponent<Renderer>().material.color == Color.red)
-            {
-                GameManager.instance.setPlayerStatus(1);
-            }
-            else if (gameObject.GetComponent<Renderer>().material.color == Color.blue)
-            {
-                GameManager.instance.setPlayerStatus(0);
-            }
-            else if (gameObject.GetComponent<Renderer>().material.color == Color.green)
-            {
-                GameManager.instance.setPlayerStatus(2);
-            }
-        }
-        else if (col.gameObject.tag == "BlueBlock")
-        {
-            if (gameObject.GetComponent<Renderer>().material.color == Color.red)
-            {
-                GameManager.instance.setPlayerStatus(2);
-            }
-            else if (gameObject.GetComponent<Renderer>().material.color == Color.blue)
-            {
-                GameManager.instance.setPlayerStatus(1);
-            }
-            else if (gameObject.GetComponent<Renderer>().material.color == Color.green)
-            {
-                GameManager.instance.setPlayerStatus(0);
-            }
-        }
-        else if (col.gameObject.tag == "GreenBlock")
-        {
-            if (gameObject.GetComponent<Renderer>().material.color == Color.red)
-            {
-                GameManager.instance.setPlayerStatus(0);
-            }
-            else if (gameObject.GetComponent<Renderer>().material.color == Color.blue)
-            {
-                GameManager.instance.setPlayerStatus(2);
-            }
-            else if (gameObject.GetComponent<Renderer>().material.color == Color.green)
-            {
-                GameManager.instance.setPlayerStatus(1);
-            }
-        }
-        else
-        {
-            GameManager.instance.setPlayerStatus(3);
-        }
-    }
+	void OnCollisionStay2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "RedBlock")
+		{
+			fly = false;
+			if (gameObject.GetComponent<Renderer>().material.color == Color.red)
+			{
+				GameManager.instance.setPlayerStatus(1);
+			}
+			else if (gameObject.GetComponent<Renderer>().material.color == Color.blue)
+			{
+				GameManager.instance.setPlayerStatus(0);
+			}
+			else if (gameObject.GetComponent<Renderer>().material.color == Color.green)
+			{
+				GameManager.instance.setPlayerStatus(2);
+			}
+		}
+		else if (col.gameObject.tag == "BlueBlock")
+		{
+			fly = false;
+			if (gameObject.GetComponent<Renderer>().material.color == Color.red)
+			{
+				GameManager.instance.setPlayerStatus(2);
+			}
+			else if (gameObject.GetComponent<Renderer>().material.color == Color.blue)
+			{
+				GameManager.instance.setPlayerStatus(1);
+			}
+			else if (gameObject.GetComponent<Renderer>().material.color == Color.green)
+			{
+				GameManager.instance.setPlayerStatus(0);
+			}
+		}
+		else if (col.gameObject.tag == "GreenBlock")
+		{
+			fly = false;
+			if (gameObject.GetComponent<Renderer>().material.color == Color.red)
+			{
+				GameManager.instance.setPlayerStatus(0);
+			}
+			else if (gameObject.GetComponent<Renderer>().material.color == Color.blue)
+			{
+				GameManager.instance.setPlayerStatus(2);
+			}
+			else if (gameObject.GetComponent<Renderer>().material.color == Color.green)
+			{
+				GameManager.instance.setPlayerStatus(1);
+			}
+		}
+		else
+		{
+			fly = true;
+			GameManager.instance.setPlayerStatus(3);
+		}
+	}
 
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "RedBlock" || col.gameObject.tag == "BlueBlock" || col.gameObject.tag == "GreenBlock")
-        {
-            fly = true;
-            GameManager.instance.setPlayerStatus(3);
-        }
-    }
+	void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "RedBlock" || col.gameObject.tag == "BlueBlock" || col.gameObject.tag == "GreenBlock")
+		{
+			fly = true;
+			GameManager.instance.setPlayerStatus(3);
+		}
+	}
 }
