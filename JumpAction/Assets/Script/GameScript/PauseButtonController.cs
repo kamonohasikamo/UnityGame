26e3a5bc36cf0796541c538735172a135c11ec9f
kamonohasikamo <kamonohasikamo@users.noreply.github.com>
2020-01-24 00:16:46 +0900

    iPhoneXR/Xsのタイマーが見切れてしまう修正＆ポーズボタンの実装

---
+++ JumpAction/Assets/Script/GameScript/PauseButtonController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButtonController : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject pauseUI;
+
+	public void onPressPauseButton()
+	{
+		pauseUI.SetActive(!pauseUI.activeSelf);
+		if (pauseUI.activeSelf)
+		{
+			Time.timeScale = 0.0f;
+		}
+	}
+}
---
+++ JumpAction/Assets/Script/GameScript/PauseWindowButtonsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseWindowButtonsController : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject pauseUI;
+	public void onPressToTitleButton()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("Start");
+	}
+
+	public void onPressReturnButton()
+	{
+		Time.timeScale = 1f;
+		pauseUI.SetActive(!pauseUI.activeSelf);
+	}
+}
