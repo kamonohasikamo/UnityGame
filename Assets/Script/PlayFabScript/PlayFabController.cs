5d9d94233462039915c7f2881e3e5dff83365136
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-08-14 22:22:00 +0900

    固定値でログインする機能の実装

---
+++ Assets/Script/PlayFabScript/PlayFabController.cs
@@ -0,0 +1,25 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabController : MonoBehaviour
+{
+	void Start()
+	{
+		PlayFabClientAPI.LoginWithCustomID(
+			new LoginWithCustomIDRequest
+			{
+				TitleId = PlayFabSettings.TitleId,
+				CustomId = "100",
+				CreateAccount = true
+			}
+		, result =>
+		{
+			Debug.Log("sucess Login!");
+		}
+		, error =>
+		{
+			Debug.Log(error.GenerateErrorReport());
+		});
+	}
+}
